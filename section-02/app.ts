b0cbcd5ed8b3775a6e69acb0e956412a1d96d958
Franklin David Macias Avellan <franklindavid.maciasavellan@gmail.com>
2022-09-18 23:38:33 +0200

    Section 02: TypeScript Basics & Basic Types

--- section-02/app.ts
+++ section-02/app.ts
@@ -1,24 +1,15 @@
-//* Union type
-function combine(input1: number | string, input2: number | string, resultConversion: string) {
-    let result: any;
-    if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number'){
-        result = +input1 + +input2
-    } else {
-        result = input1.toString() + input2.toString();
-    }
-    return result;
-    /*if (resultConversion === 'as-number') {
-        return +result;
-    } else {
-        return result.toString();
-    }*/
+let userInput: unknown;
+let userName: string;
+
+userInput = 5;
+userInput = 'David';
+if (typeof userInput === 'string') {
+    userName = userInput;
 }
 
-const combinedAges = combine(30, 26, 'as-number');
-console.log(combinedAges);
+//* Utility function
+function generateError(message: string, code: number): never {
+    throw {message: message, errorCode: code}
+}
 
-const combinedStringAges = combine('30', '26', 'as-number');
-console.log(combinedStringAges);
-
-const combinedNames = combine('David', 'Ana', 'as-text');
-console.log(combinedNames);
+generateError('An error ocurred!', 500);

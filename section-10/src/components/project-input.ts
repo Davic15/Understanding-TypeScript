e853f464fc8dc62cef1fc74b69b23bd67d8fe1c2
Franklin David Macias Avellan <franklindavid.maciasavellan@gmail.com>
2022-09-27 15:56:21 +0200

    Section 10: Modules & Namespaces

--- section-10/src/components/project-input.ts
+++ section-10/src/components/project-input.ts
@@ -2,6 +2,11 @@ import { Component } from "./base-component.js";
 import { Validatable, validate } from "../util/validation.js";
 import { autobind } from "../decorators/autobind.js";
 import { projectState } from "../state/project-state.js";
+
+//* import * as Validate from "../util/validation.js";
+//* Validate.Validatable
+//* import { autobind as Autobind } from "../decorators/autobind.js";
+
 //* ProjectInput class
 export class ProjectInput extends Component<HTMLDivElement, HTMLFormElement> {
     //* HTML has diferent elements even here.
